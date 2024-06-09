556f34602f963b24aefd356c418cdca275acf07a
Nick Ganter <nickganter91@gmail.com>
2024-06-09 18:40:17 -0500

    Uncomment HSTS code.

--- src/Hamdlebot/Hamdlebot.Web/Program.cs
+++ src/Hamdlebot/Hamdlebot.Web/Program.cs
@@ -77,11 +77,11 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 var forwardedHeadersOptions = new ForwardedHeadersOptions
 {
