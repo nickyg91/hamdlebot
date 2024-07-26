78ee2301a551a1c7f96327d7c2c2f4a17a35d715
Nick Ganter <nickganter91@gmail.com>
2024-07-26 13:17:24 -0500

    Fixed path - was somehow unincluded

---
+++ src/Hamdlebot/Hamdlebot.Web/ClientApp/src/components/dashboard/features/bot-log/LogConsole.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { ILogMessage } from '@/components/dashboard/models/log-message.interface';
+defineProps<{ messages: ILogMessage[] }>();
+</script>
+<template>
+  <div></div>
+</template>
+<style scoped></style>
